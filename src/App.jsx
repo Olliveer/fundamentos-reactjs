f4c88afdc2c819b7f2efa96eec85b28664ab9ee4
José Oliveira <j.oliveira90@live.com>
2022-06-05 14:58:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+
+import './global.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+    </div>
+  );
+}
+
+export default App;
