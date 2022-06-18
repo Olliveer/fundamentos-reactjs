88b2a14f0539a74801dd0f2b23b4d2aa83fe6270
José Oliveira <j.oliveira90@live.com>
2022-06-18 12:47:37 -0300

    add comment component

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/50416284?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>José Oliveira</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:00:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito Bom, parabéns</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
